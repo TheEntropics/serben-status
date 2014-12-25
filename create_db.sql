56576e8acb47d900450c3ea5f75f742de1fb1d1e
Edoardo Morassutto <gianluigi988@gmail.com>
2014-12-25 15:56:26 +0100

    Aggiunta query per creare il db

---
+++ create_db.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dic 25, 2014 alle 15:55
+-- Versione del server: 5.5.40-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `serben_status`
+--
+CREATE DATABASE IF NOT EXISTS `serben_status` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `serben_status`;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `pings`
+--
+
+DROP TABLE IF EXISTS `pings`;
+CREATE TABLE IF NOT EXISTS `pings` (
+`ID` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `up` tinyint(1) NOT NULL,
+  `ping` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `services`
+--
+
+DROP TABLE IF EXISTS `services`;
+CREATE TABLE IF NOT EXISTS `services` (
+`ID` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `service` varchar(50) NOT NULL,
+  `status` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `sysInfo`
+--
+
+DROP TABLE IF EXISTS `sysInfo`;
+CREATE TABLE IF NOT EXISTS `sysInfo` (
+`ID` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `cpu` float NOT NULL,
+  `ram` float NOT NULL,
+  `uptime` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pings`
+--
+ALTER TABLE `pings`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `sysInfo`
+--
+ALTER TABLE `sysInfo`
+ ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pings`
+--
+ALTER TABLE `pings`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sysInfo`
+--
+ALTER TABLE `sysInfo`
+MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
